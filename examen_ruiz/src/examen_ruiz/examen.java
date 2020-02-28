24edd3f912c52a1268254f67694cbfc136f16a68
xfresisuii <marinaruizpajares@gmail.com>
2020-02-28 09:39:07 +0100

    nova branca cambiat

--- examen_ruiz/src/examen_ruiz/examen.java
+++ examen_ruiz/src/examen_ruiz/examen.java
@@ -5,16 +5,12 @@ public class examen {
 	public static void main(String[] args) {
 		printOne();
 		printOne();
-		printTwo();
+		
 	}
 	
 	public static void printOne() {
 		System.out.print("Helo world");
 	}
 	
-	public static void printTwo() {
-		printOne();
-		printOne();
-	}
-
+	
 }
